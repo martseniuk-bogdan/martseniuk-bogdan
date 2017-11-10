99c86f13f78536514c3f586c659522c3cd0b70e1
martseniuk-bogdan <ohduda69@gmail.com>
2017-11-10 21:37:47 +0300

    Анонимный класс для интерфейса принтейбл

--- src/com/company/lessons15/user/MainPrintable.java
+++ src/com/company/lessons15/user/MainPrintable.java
@@ -4,7 +4,7 @@ import com.company.lessons11.taskPrintable.Printable;
 
 public class MainPrintable {
     public static void main(String[] args) {
-        Printable printable=new Printable() {
+        Printable printable = new Printable() {
             @Override
             public void print() {
                 System.out.println("smth text");
