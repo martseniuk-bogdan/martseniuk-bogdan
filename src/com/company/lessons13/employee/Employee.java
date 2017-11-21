89708e27fed2754dc7ca1f96b7ef1d0b6aa1bb6d
martseniuk-bogdan <ohduda69@gmail.com>
2017-11-21 21:33:54 +0200

    14.6 Full Report - done

--- src/com/company/lessons13/employee/Employee.java
+++ src/com/company/lessons13/employee/Employee.java
@@ -1,5 +1,7 @@
 package com.company.lessons13.employee;
 
+import java.util.Date;
+
 /**
  * Создать класс Employee, у которого есть переменные класса - fullname, salary. Создать массив содержащий несколько объектов этого типа.
  * Создать класс Report, который будет содержать статический метод generateReport, в котором выводится информация о зарплате всех сотрудников.
@@ -8,13 +10,15 @@ package com.company.lessons13.employee;
 public class Employee {
     private String fullname;
     private double salary;
+    private Date salaryDate;
 
     public Employee() {
     }
 
-    public Employee(String fullname, double salary) {
+    public Employee(String fullname, double salary, Date salaryDate) {
         this.fullname = fullname;
         this.salary = salary;
+        this.salaryDate = salaryDate;
     }
 
     public String getFullname() {
@@ -33,6 +37,14 @@ public class Employee {
         this.salary = salary;
     }
 
+    public Date getSalaryDate() {
+        return salaryDate;
+    }
+
+    public void setSalaryDate(Date salaryDate) {
+        this.salaryDate = salaryDate;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,7 +53,8 @@ public class Employee {
         Employee employee = (Employee) o;
 
         if (Double.compare(employee.salary, salary) != 0) return false;
-        return fullname != null ? fullname.equals(employee.fullname) : employee.fullname == null;
+        if (fullname != null ? !fullname.equals(employee.fullname) : employee.fullname != null) return false;
+        return salaryDate != null ? salaryDate.equals(employee.salaryDate) : employee.salaryDate == null;
     }
 
     @Override
@@ -51,6 +64,7 @@ public class Employee {
         result = fullname != null ? fullname.hashCode() : 0;
         temp = Double.doubleToLongBits(salary);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (salaryDate != null ? salaryDate.hashCode() : 0);
         return result;
     }
 
@@ -59,6 +73,7 @@ public class Employee {
         return "Employee{" +
                 "fullname='" + fullname + '\'' +
                 ", salary=" + salary +
+                ", salaryDate=" + salaryDate +
                 '}';
     }
 }
--- src/com/company/lessons13/employee/Report.java
+++ src/com/company/lessons13/employee/Report.java
@@ -1,17 +1,22 @@
 package com.company.lessons13.employee;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Report {
     public static void main(String[] args) {
         Employee[] employees = new Employee[3];
-        employees[0] = new Employee("Ященко ", 1500.501);
-        employees[1] = new Employee("Марценюк ", 1500.101);
-        employees[2] = new Employee("Щербинина ", 3000.000);
+        employees[0] = new Employee("Ященко ", 1500.501, new Date(1500000000000L));
+        employees[1] = new Employee("Марценюк ", 1500.101, new Date(1505500000000L));
+        employees[2] = new Employee("Щербинина ", 3000.000, new Date(1500500000000L));
         generateReport(employees);
     }
 
     public static void generateReport(Employee[] employees) {
+        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
         for (Employee employee : employees) {
-            System.out.printf("Работник: %s получает зарплату: %5.2f$\n", employee.getFullname(), employee.getSalary());
+            String s = df.format(employee.getSalaryDate());
+            System.out.printf("Работник: %s получает зарплату: %5.2f$ такого числа- %s\n", employee.getFullname(), employee.getSalary(), s);
         }
     }
 }
